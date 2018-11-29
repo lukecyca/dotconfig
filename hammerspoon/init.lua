2c5228398d7fe8b2e5e5ac43ccfba0d8df9365e5
Luke Cyca <me@lukecyca.com>
2018-11-28 16:25:34 -0800

    Hammerspoon improvements

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,45 +1,35 @@
-mash = {"shift", "ctrl"}
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
 
+function newWindow(appName, menuItemName)
+  local app = hs.appfinder.appFromName(appName)
 
--- Reloads hammerspoon config
-hs.hotkey.bind(mash, "R", function()
-  hs.reload()
-  hs.alert.show("Config loaded")
-end)
+  if app ~= nil then
+    local currWin = app:mainWindow()
+    app:selectMenuItem(menuItemName)
 
+    -- Wait until the window opens
+    local tries = 0
+    repeat
+      os.execute("sleep 0.1")
+      tries = tries + 1
+    until currWin ~= app:mainWindow() or tries > 5
+  end
 
--- Opens a new application window in the current space
+  hs.application.launchOrFocus(appName)
+
+end
+
 function bindNewWin(key, appName, menuItemName)
-  hs.hotkey.bind(mash, key, function()
-    local app = hs.appfinder.appFromName(appName)
-
-    if app ~= nil then
-      local currWin = app:mainWindow()
-      app:selectMenuItem(menuItemName)
-
-      -- Wait until the window opens
-      local tries = 0
-      repeat
-        os.execute("sleep 0.1")
-        tries = tries + 1
-      until currWin ~= app:mainWindow() or tries > 5
-    end
-
-    hs.application.launchOrFocus(appName)
+  hs.hotkey.bind({"cmd", "alt", "ctrl"}, key, function()
+    newWindow(appName, menuItemName)
   end)
 end
 
-bindNewWin("f1", "Firefox", "New Window")
+bindNewWin("f1", "Safari", "New Window")
 bindNewWin("f2", "Finder", "New Finder Window")
 bindNewWin("f3", "Mail", "New Message")
 bindNewWin("f4", "iTerm", "New Window")
 
-bindNewWin("f5", "MacVim", "New Window")
+bindNewWin("f5", "Code", "New Window")
 bindNewWin("f6", "Soulver", "New")
-
-
--- Starts screensaver and locks the screen
-local screensaver = "/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
-hs.hotkey.bind(mash, "escape", function()
-  hs.application.launchOrFocus(screensaver)
-end)
